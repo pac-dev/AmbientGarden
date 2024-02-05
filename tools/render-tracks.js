adf8b1b03aec52068d4c3a3a77d35e1fe5cea1c3
pac-dev <pierre@osar.fr>
2024-02-04 19:22:23 -0700

    Make CLI renderer crash slighly less
    The memory used by modules loaded by Deno cannot be recovered, leading
    to an inevitable memory leak in the CLI renderer. This is remedied by
    using multi-process rendering.

--- tools/render-tracks.js
+++ tools/render-tracks.js
@@ -1,5 +1,5 @@
 import { exists, parse, path } from './deps.js';
-import { loadTrack, createRenderer } from '../../Teasynth/teasynth.js';
+import { MultiRenderer } from '../../Teasynth/teasynth.js';
 import { beaconRecords } from '../web/ag_static/js/beacons/beaconRecords.js';
 
 const baseDir = path.join(path.fromFileUrl(import.meta.url), '..', '..');
@@ -7,6 +7,10 @@ const tracksDir = path.join(baseDir, 'tracks');
 const webDir = path.join(baseDir, 'web');
 
 const args = parse(Deno.args);
+const clamp01 = (x) => Math.max(0, Math.min(1, x));
+const xf = x => -1 * Math.pow(-0.5 * Math.cos((clamp01(x) + 1)*Math.PI) + 0.5, 1.772) + 1;
+const errorDetail = `
+Tracks should call "graph.setSplicePoint" when reaching desired splice-points.`;
 
 /** @param {import('../web/ag_static/js/beacons/beaconRecords.js').BeaconRecord} beacon */
 const renderBeacon = async beacon => {
@@ -17,28 +21,27 @@ const renderBeacon = async beacon => {
 		return;
 	}
 	console.log('rendering: ' + beacon.desc);
-	let inDur = args.intro ?? 12;
-	let loopDur = args.loop ?? 18;
+	Deno.mkdirSync(path.dirname(introPath), {recursive: true});
 	const xfDur = args.xf ?? 2;
-	const track = await loadTrack(path.join(tracksDir, beacon.trackName, 'main.js'));
-	track.setParams(beacon.trackParams);
-	const r = createRenderer(track);
-	const inPipe = await r.addOutput(introPath);
-	if (track.host.willInterupt) {
-		delete track.host.willInterupt;
-		inDur = 666;
-		loopDur = 666;
-	}
-	await r.render(inDur);
-	r.fadeOut(inPipe, xfDur);
-	const loopPipe = await r.addOutput(loopPath);
-	r.fadeIn(loopPipe, xfDur);
+	const r = new MultiRenderer();
+	const track = r.addTrack(path.join(tracksDir, beacon.trackName, 'main.js'), beacon.trackParams);
+	const introFile = await r.addOutput(introPath);
+	await r.render(66, { stopAtSplicePoint: true });
+	const introEnd = r.position;
+	if (introEnd > 60) throw new Error('Did not get any interrupt after 60s of intro.'+errorDetail);
+	const loopFile = await r.addOutput(loopPath);
+ 	introFile.mixFn = (x, t) => x*xf(1 - (t - introEnd)/xfDur);
+	loopFile.mixFn = (x, t) => x*xf((t - introEnd)/xfDur);
 	await r.render(xfDur);
-	await r.removeOutput(inPipe);
-	await r.render(loopDur);
-	r.fadeOut(loopPipe, xfDur);
+	await r.removeOutput(introFile);
+	await r.render(126, { stopAtSplicePoint: true });
+	const loopEnd = r.position;
+	if (loopEnd > 120) throw new Error('Did not get any interrupt after 120s of loop.'+errorDetail);
+	loopFile.mixFn = (x, t) => x*clamp01(1 - (t - loopEnd)/xfDur);
 	await r.render(xfDur);
-	await r.removeOutput(loopPipe);
+	await r.removeOutput(loopFile);
+	r.removeTrack(track);
+	await r.finalize();
 };
 
 for (let b of beaconRecords) {
